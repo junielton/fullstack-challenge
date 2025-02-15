1ab037b42a18b342bff38124b970356389811a4e
junielton <junyellton@gmail.com>
2025-02-14 21:57:00 -0300

    feat: update HomeView to include navigation and UsersList component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import ApiTest from "@/components/ApiTest.vue";
-</script>
-
 <template>
-  <main>
-    <api-test></api-test>
-  </main>
+  <div class="min-h-screen">
+    <nav class="fixed top-0 bg-white text-2xl font-bold w-full z-10 shadow-xl">
+      <div class="max-w-screen-xl mx-auto p-4">
+        <h1>Users weather</h1>
+      </div>
+    </nav>
+    <main class="max-w-screen-xl mx-auto p-4 mt-20">
+      <hr />
+      <UsersList />
+    </main>
+  </div>
 </template>
+
+<script setup lang="ts">
+import UsersList from '@/components/UsersList.vue'
+</script>
